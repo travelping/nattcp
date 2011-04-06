4a5135bd969b511f5824d4d3d34332700be9702d
Robin Haberkorn <rh@travelping.com>
2011-04-06 23:47:44 +0200

    added udp-climber tool to determine maximum UDP throughputs
     * updated makefile, prepared for win32/64 targets that add necessary
    flags automatically

--- Makefile
+++ Makefile
@@ -1,12 +1,30 @@
-CC ?= gcc
+CC := gcc
+CFLAGS := -Wall -O3
+EXTRA_CPPFLAGS := -DUDP_FLIP
 
-CFLAGS ?= -Wall -g -O0
-CPPFLAGS += -DUDP_FLIP
+LUAC := luac
+LUACFLAGS := -s
 
-all : nattcp
+MANIFEST := nattcp$(EXEEXT) udp-climber
 
-nattcp : nattcp.c
-	$(CC) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS) -o $@ $<
+all : $(MANIFEST)
+
+win32:
+	make CC="$(CC)" \
+	     CFLAGS="$(CFLAGS)" CPPFLAGS="$(CPPFLAGS)" LDFLAGS="$(LDFLAGS)" \
+	     EXEEXT=".exe" all
+win64:
+	make CC="$(CC)" \
+	     CFLAGS="$(CFLAGS)" CPPFLAGS="$(CPPFLAGS)" LDFLAGS="$(LDFLAGS)" \
+	     EXEEXT=".exe" all
+
+nattcp$(EXEEXT) : nattcp.c
+	$(CC) $(CFLAGS) $(CPPFLAGS) $(EXTRA_CPPFLAGS) $(LDFLAGS) -o $@ $<
+
+udp-climber : udp-climber.lua
+	echo "#!/usr/bin/lua" >$@
+	$(LUAC) $(LUACFLAGS) -o - $< >>$@
+	chmod +x $@
 
 clean:
-	rm -f nattcp *.o
+	rm -f *.o $(MANIFEST)
---
+++ udp-climber.lua
@@ -0,0 +1,115 @@
+#!/usr/bin/lua
+
+local function printf(...) io.write(string.format(...)) end
+local function errorf(...) error(string.format(...), 2) end
+
+local function usage(r)
+	printf(
+[=[Runs a succession of nattcp UDP tests to determine the maximum UDP throughput.
+Usage:
+%s [options] server
+options:
+	-h		Print this help
+	-D		Test downstream throughput (default)
+	-U		Test upstream throughput
+	-f <rate>	Rate in kbps to start with (default: 500)
+	-t <rate>	Rate in kbps that will not be exceeded (default: 20000)
+	-s <rate>	Rate in kbps to increase in each step (default: 50)
+	-T <time>	Transmit time in seconds for each test (default: 10)
+The NATTCP environment variable specifies the nattcp binary to use (default: "nattcp").
+]=], arg[0])
+	os.exit(r or 1) -- unsuccessful by default
+end
+
+-- defaults
+local binary = os.getenv("NATTCP") or "nattcp"
+local downstream = true
+
+local time = 10 -- seconds
+local start_rate = 500 -- kbps
+local limit_rate = 20000 -- kbps
+local step_rate = 50 -- kbps
+
+local host = arg[#arg] -- required
+if #arg < 1 or host:sub(1, 1) == "-" then usage() end
+
+-- parse command line
+local i = 1
+while i < #arg-1 do
+	if arg[i] == "-h" then usage(0)
+	elseif arg[i] == "-D" then downstream = true
+	elseif arg[i] == "-U" then downstream = false
+	elseif arg[i] == "-f" then
+		i = i + 1
+		start_rate = tonumber(arg[i])
+		if not start_rate or start_rate < 1 then usage() end
+	elseif arg[i] == "-t" then
+		i = i + 1
+		limit_rate = tonumber(arg[i])
+		if not limit_rate or limit_rate < 1 then usage() end
+	elseif arg[i] == "-s" then
+		i = i + 1
+		step_rate = tonumber(arg[i])
+		if not step_rate or step_rate < 1 then usage() end
+	elseif arg[i] == "-T" then
+		i = i + 1
+		time = tonumber(arg[i])
+		if not time or time < 1 then usage() end
+	end
+	i = i + 1
+end
+if start_rate > limit_rate then usage() end
+
+local function test()
+	local i = 1
+	local prev_results = {rate_Mbps = 0}
+
+	for rate = start_rate, limit_rate, step_rate do
+		local cmd = downstream and
+			    string.format("%s -r -u -F -fparse -T%d -R%d %s", binary, time, rate, host) or
+			    string.format("%s -t -u -fparse -T%d -R%d %s", binary, time, rate, host)
+		printf("%d: executing: %s\n", i, cmd)
+
+		local nattcp, msg = io.popen(cmd)
+		if not nattcp then
+			errorf("Cannot execute \"%s\": %s", binary, msg or "(nil)")
+		end
+
+		local out = nattcp:read("*a")
+		nattcp:close()
+		if not out or #out == 0 then
+			errorf("Invalid results while executing \"%s\".", binary)
+		end
+
+		local results = {}
+		for key, value in out:gmatch("([%w_]+)=([%d.]+)") do
+			results[key] = tonumber(value)
+		end
+
+		printf("%d: rx rate = %fmbps, loss = %d%%\n", i,
+		       results.rate_Mbps, results.data_loss*100)
+
+		if results.rate_Mbps <= prev_results.rate_Mbps or
+		   results.data_loss > 0.1 then
+			printf("Receive rate does no longer increase or there are excessive drops.\n"..
+			       "Best result was: %fmbps/%fkbps/%dkibits.\n",
+			       prev_results.rate_Mbps,
+			       prev_results.rate_Mbps*1000,
+			       prev_results.rate_Mbps*1000/1024)
+			os.exit(0)
+		end
+
+		prev_results = results
+		i = i + 1
+	end
+end
+
+local status, msg = pcall(test)
+if not status then
+	printf("Aborted: %s\n", msg)
+	os.exit(1)
+end
+
+printf("No peformance decrease detectable. Please rerun with limit > %dkbps.\n",
+       limit_rate)
+
